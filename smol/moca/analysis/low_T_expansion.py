2c91d87a0174c30f95cf63eb56ce0cd1b040bbbf
Ronald Kam <kamronald@berkeley.edu>
2022-10-20 00:13:23 -0700

    Fix duplication of excitations in low T expansion
    Fix loops such that there will be no swaps of the same species. Also ensured that degeneracies will be suitably accounted for.

--- smol/moca/analysis/low_T_expansion.py
+++ smol/moca/analysis/low_T_expansion.py
@@ -2,6 +2,9 @@
 energy at low temperatures, where it is likely that MC will get "stuck" on ground
 states, yielding unconverged thermodynamic quantities.
 """
+
+__author__ = "Ronald Kam"
+
 import copy
 import math
 import numpy as np
@@ -12,17 +15,17 @@ from smol.moca.sublattice import Sublattice
 from smol.constants import kB
 
 
-def canonical_low_t_exp(ensemble, gs_occu):
-    """ Implement low temperature expansion of a binary system on canonical ensemble.
-    Include all single swap perturbations from the ground state in the partition
-    function.
+def gen_canon_excitations(ensemble, gs_occu):
+    """ Generate all single swap perturbations from the ground state to include in a
+    canonical partition function.
 
     Args:
         processor:
-        gs_occu: Encoded occupancy of ground state structure
+        gs_occu: Encoded occupancy array of ground state structure
 
     Returns:
-
+        all_swap_energies (Dict): dictionary mapping energy of excitation from a ground
+        state to its multiplicity
     """
     gs_struc = ensemble.processor.structure_from_occupancy(gs_occu,
                                                            include_vacancies=True
@@ -41,9 +44,27 @@ def canonical_low_t_exp(ensemble, gs_occu):
         for en, mult in subl_swap_energies.items():
             if en in all_swap_energies:
                 all_swap_energies[en] += mult
-            all_swap_energies[en] = mult
+            else:
+                all_swap_energies[en] = mult
 
     return all_swap_energies
+
+
+def eval_canon_z(energy_mult_d, temp, min_e):
+    """ Evaluate canonical partition function at a temperature, given information on
+    energies. Values are centered on the ground state energy, such that exponential
+    functions can be nicely computed.
+
+    Args:
+        energy_mult_d (Dict): {energy: multiplicity}
+
+    Returns:
+        (float) evaluated value of partition function at a given temperature
+
+    """
+    return 1 + sum(
+        [math.exp(-(e-min_e) / (kB*temp)) * mult for e, mult in energy_mult_d.items()]
+    )
 
 
 def get_subl_equiv_sites(sublattice, symmetrized_structure):
@@ -51,8 +72,8 @@ def get_subl_equiv_sites(sublattice, symmetrized_structure):
     grouped by species.
 
     Args:
-        sublattice:
-        symmetrized_structure:
+        sublattice: active sublattice in a given ensemble
+        symmetrized_structure: SymmetrizedStructure (from Pymatgen)
 
     Returns:
         Dictionary mapping species to list of list of equivalent sites
@@ -65,10 +86,12 @@ def get_subl_equiv_sites(sublattice, symmetrized_structure):
         if spec not in sublattice.species:
             continue
 
+        relevant_inds = [ind for ind in equiv_inds if ind in sublattice.sites]
+
         if spec not in species_site_map:
-            species_site_map[spec] = [equiv_inds]
+            species_site_map[spec] = [relevant_inds]
         else:
-            species_site_map[spec].append(equiv_inds)
+            species_site_map[spec].append(relevant_inds)
 
     return species_site_map
 
@@ -88,13 +111,13 @@ def gen_swap_energies(species_site_map, processor, occu):
     """
     all_species = [s for s in species_site_map.keys()]
     swap_en_mults = {}
-    for i in range(len(all_species)):
+    for i in range(len(all_species)-1):
         for equiv_sites_i in species_site_map[all_species[i]]:
             # Perform swaps with all the other sites, keeping in mind not to duplicate
             site_i_ind = equiv_sites_i[0]
             # only need to swap one of these sites, the rest are symmetrically equiv.
             multiplicity = len(equiv_sites_i)
-            remaining_species = all_species[i:]
+            remaining_species = all_species[i+1:]
             sites_to_swap = [
                 sites for species, sites_list in species_site_map.items()
                 for sites in sites_list if species in remaining_species
@@ -102,27 +125,27 @@ def gen_swap_energies(species_site_map, processor, occu):
             for equiv_sites_j in sites_to_swap:
                 for site_j_ind in equiv_sites_j:
                     swap_en = compute_swap(processor, site_i_ind, site_j_ind, occu)
-                    swap_en_mults[swap_en] = multiplicity
+                    if swap_en in swap_en_mults:
+                        swap_en_mults[swap_en] += multiplicity
+                    else:
+                        swap_en_mults[swap_en] = multiplicity
 
     return swap_en_mults
 
 
-def canon_partition_function(energy_mult_d, temp, min_e):
-    """ Evaluate canonical partition function at a temperature, given list of energies.
+def compute_swap(processor, s1_ind, s2_ind, occu):
+    """ Compute energy after a given swap.
 
     Args:
-        energy_mult_d: {energy: multiplicity}
+        processor:
+        s1_ind (int): site index 1
+        s2_ind (int): site index 2
+        occu (np.array of ints): encoded occupancy array
 
     Returns:
-        evaluated canonical partition function at a temperature
+        swap_energy (float): energy of new configuration after swap.
 
     """
-    return 1 + sum(
-        [math.exp(-(e-min_e) / (kB*temp)) * mult for e, mult in energy_mult_d.items()]
-    )
-
-
-def compute_swap(processor, s1_ind, s2_ind, occu):
     new_occu = copy.deepcopy(occu)
     new_occu[s1_ind] = occu[s2_ind]
     new_occu[s2_ind] = occu[s1_ind]
